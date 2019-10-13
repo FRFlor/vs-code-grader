e0fb2e92c6fc3757a104be0e9cacaadf5bb5514f
Felipe Flor <f.flor@vehikl.com>
2019-10-12 20:49:19 -0400

    Implement history and file sections of the nav bar

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,40 +1,72 @@
 <template>
-  <nav>
-    <div class="switch-tabs flex">
-      <v-button alt="arrow-left">
-        <img src="../assets/arrowLeft.svg" alt="go-to-previous-tab">
-      </v-button>
-      <v-button alt="arrow-left">
-        <img src="../assets/caret.svg" alt="see-tabs">
-      </v-button>
-      <v-button alt="arrow-right">
-        <img src="../assets/arrowLeft.svg" style="transform: scaleX(-1);" alt="go-to-next-tab">
-      </v-button>
-    </div>
-  </nav>
+    <nav class="flex">
+        <div class="switch-tabs flex">
+            <v-button>
+                <img src="../assets/arrowLeft.svg" alt="go to previous tab">
+            </v-button>
+            <v-button>
+                <img src="../assets/caret.svg" alt="see open tabs">
+            </v-button>
+            <v-button>
+                <img src="../assets/arrowLeft.svg" style="transform: scaleX(-1);" alt="go to next tab">
+            </v-button>
+        </div>
+        <img src="../assets/divisor.svg" alt="divisor">
+        <div class="file-options flex">
+            <v-button>
+                <img src="../assets/newFile.svg" alt="create new file">
+            </v-button>
+            <v-button>
+                <img src="../assets/caret.svg" alt="new report">
+            </v-button>
+            <v-button>
+                <img src="../assets/save.svg" alt="save">
+            </v-button>
+            <v-button>
+                <img src="../assets/saveAs.svg" alt="save as">
+            </v-button>
+        </div>
+        <img src="../assets/divisor.svg" alt="divisor">
+        <div class="history flex">
+            <v-button>
+                <img src="../assets/back.svg" alt="previous">
+            </v-button>
+            <v-button>
+                <img src="../assets/caret.svg" alt="see previous options">
+            </v-button>
+            <v-button>
+                <img src="../assets/back.svg" style="transform: scaleX(-1);" alt="next">
+            </v-button>
+            <v-button>
+                <img src="../assets/caret.svg" alt="see next options">
+            </v-button>
+        </div>
+        <img src="../assets/divisor.svg" alt="divisor">
+    </nav>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import VButton from "@/components/VButton.vue";
-@Component({
-  components: {VButton},
-})
-export default class TopNav extends Vue {
-}
+    import {Component, Vue} from "vue-property-decorator";
+    import VButton from "@/components/VButton.vue";
+
+    @Component({
+        components: {VButton},
+    })
+    export default class TopNav extends Vue {
+    }
 </script>
 
 <style lang="scss" scoped>
-  @import "../app";
+    @import "../app";
 
-  nav {
-    background-color: $vs_dark_gray;
-    display: flex;
-    height: 35px;
-    width: 100px;
-  }
+    nav {
+        background-color: $vs_dark_gray;
+        display: flex;
+        height: 35px;
+    }
 
-  img {
-    pointer-events: none;
-  }
+    img {
+        pointer-events: none;
+    }
+
 </style>
