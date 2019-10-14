c4e2416175a95a7cdc422e6a73b7384af36ced4a
Felipe Flor <f.flor@vehikl.com>
2019-10-14 16:55:03 -0400

    Stub-out warning buttons

--- src/components/PrepareReport.vue
+++ src/components/PrepareReport.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex justify-between">
-        <div class="prepare-report">
+    <div class="flex justify-between prepare-report">
+        <div class="action-buttons-wrapper">
             <h2>Prepare Report:</h2>
             <v-button class="blue-button" @click="previewDocument">
                 <vue-svg name="play"/>
@@ -16,7 +16,15 @@
             </v-button>
         </div>
         <div class="warning-display">
-
+            <h2>There are x warnings:</h2>
+            <div class="warning-buttons-container">
+                <div v-for="i in 6">
+                    <v-button class="warning-button" @click="previewDocument">
+                        <span>You suck</span>
+                        <vue-svg name="play"/>
+                    </v-button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -56,8 +64,34 @@
 
     .prepare-report {
         margin-top: 0.5rem;
+    }
+
+    .action-buttons-wrapper {
         display: flex;
         flex-direction: column;
+    }
+
+    .warning-buttons-container {
+        max-height: 7rem;
+        overflow-y: auto;
+    }
+
+    .warning-button {
+        background-color: $gray;
+        color: $vs_white;
+        width: 18rem;
+        padding: 0.3rem 1rem 0.3rem 0.5rem;
+        border-radius: 0.3rem;
+        margin-bottom: 0.3rem;
+
+        &:hover {
+            cursor: pointer;
+        }
+
+        span {
+            flex: 1;
+            text-align: start;
+        }
     }
 
     .blue-button {
